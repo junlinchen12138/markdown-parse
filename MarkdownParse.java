527189f5302a083b5c1ce70944fa2e1a9d79aa13
junlinchen12138 <97692984+junlinchen12138@users.noreply.github.com>
2022-03-16 22:04:22 -0700

    t

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,40 +3,85 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.io.File;
+
+
+
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        System.out.println(currentIndex);
-        int lastClosedParen = markdown.lastIndexOf(")");
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextOpeCarrot = markdown.indexOf("<", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int nextCloseCarrot = markdown.indexOf(">", nextOpenCarrot);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenCarrot < nextOpenBracket){
-                carrotLink = true;
-                toReturn.add(markdown.substring(nextOpenCarrot + 1, nextCloseCarrot));
-            } else{
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
-            currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
-            if (closeParen == lastCloseParen){
-                break;
-            }
-        }
-        return toReturn;
+    public static boolean containsBrackets(String link) {
+        if(!link.contains("[")){return false;}
+        if(!link.contains("]")){return false;}
+        if(!link.contains("(")){return false;}
+        if(!link.contains(")")){return false;}
+        return true;
     }
+    public static boolean containsSpace(String link) {
+        return link.contains(" ");
+    }
+
+    //Copied from https://github.com/ucsd-cse15l-w22/markdown-parse/blob/1f7485854d92deb69c1bf8290c267fee27371d17/MarkdownParse.java#L32-L50
+    //For Lab 9
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            String filename = p.toString();
+            if(containsBrackets(Files.readString(p))){
+                System.out.println(filename + " does contain the needed brackets.");
+            }else{
+                System.out.println(filename + " does not contain the needed brackets.");
+            }
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
+    public static ArrayList<String> getLinks(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node node = parser.parse(markdown);
+        LinkCountVisitor visitor = new LinkCountVisitor();
+        node.accept(visitor);
+        return visitor.links;
+    }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
+        Map<String, List<String>> links = getLinks(new File(args[0]));
         System.out.println(links);
+        
+    }
+}
+
+//this class can be defined anywhere in the file
+class LinkCountVisitor extends AbstractVisitor {
+    int linkCount = 0;
+    ArrayList<String> links = new ArrayList<>();
+ 
+    @Override
+    public void visit(Link link) {
+        // This is called for all Link nodes.
+
+        // Count links
+        linkCount += 1;
+
+        //add link to list
+        links.add(link.getDestination());
+ 
+        // Descend into children
+        visitChildren(link);
     }
 }
--- test-file2.md
+++ test-file2.md
@@ -4,3 +4,10 @@
 [another link!](some-page.html)
 
 some paragraph text after the links
+
+@Test
+    public void testFile3() throws IOException {
+        String contents= Files.readString(Path.of("./lab8test3.md"));
+        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
